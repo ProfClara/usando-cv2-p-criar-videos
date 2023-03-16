cef9dabb1166c20855482b0572fb5b4e09e11c50
Teacher Claire (Clara) <91446846+ProfClara@users.noreply.github.com>
2023-03-16 19:05:40 -0300

    Add files via upload

---
+++ ativ do aluno.py
@@ -0,0 +1,19 @@
+#import cv2
+
+#crie uma  variável que guarda as imagens fornecidas pela webcam  do  pc
+
+#video=cv2.VideoCapture(meuvideo.mp4)
+
+"""Etapa para evitar dor de cabeça: 
+Peça p/ o computador avisar caso algo dê errado 
+na captura do vídeo"""
+
+
+# capturando a altura do vídeo
+
+# capturando a largura do vídeo
+
+# capturando os frames por segundo do vídeo
+
+
+#Loop para ler os frames depois exibir(em forma de vídeo)
